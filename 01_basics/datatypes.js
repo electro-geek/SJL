c5d2013c8690faa603a147ac02a18b1f633e7519
Mritunjay Sharma <57140997+electro-geek@users.noreply.github.com>
2024-05-05 08:11:16 +0000

    Datatype commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs not browser
+
+let name = "Mritunjay"
+let age = 18
+let isLoggedIn = false
+let state;
+// datatypes
+//number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+//null => standalone value and also datatype
+//undefined => e.g when variable is decalred and no value is assigned to it
+// symbol => uniqueness
+
+//object
+
+console.log(typeof "Mritunjay")
+console.log(typeof null)//gives u object
+console.log(typeof state)//undefined
